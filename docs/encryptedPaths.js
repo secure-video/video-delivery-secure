8293d01e332f194b6860da1e50608ec37ead1b9a
28475-train <rmiyazk.316@gmail.com>
2026-07-18 08:41:53 +0900

    Create encryptedPaths.js

---
+++ docs/encryptedPaths.js
@@ -0,0 +1,11 @@
+// これは例。実際は嶺がローカルで暗号化して作る
+const encryptedPaths = {
+  gingasai: "ここに AES で暗号化した文字列",
+  gintalk_day1: "ここに AES で暗号化した文字列",
+  gintalk_day2: "ここに AES で暗号化した文字列",
+  gintalk_day3: "ここに AES で暗号化した文字列",
+  gintalk_day4: "ここに AES で暗号化した文字列",
+  sansoukai: "ここに AES で暗号化した文字列",
+  others1: "ここに AES で暗号化した文字列",
+  others2: "ここに AES で暗号化した文字列"
+};
